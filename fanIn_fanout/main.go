371040d93ab6042205c25a4a33f863cdaefa9320
Vladimirmoscow84 <ccr1@yandex.ru>
2025-09-05 10:39:51 +0300

    add function done

--- fanIn_fanout/main.go
+++ fanIn_fanout/main.go
@@ -27,6 +27,33 @@ func generator(ctx context.Context, data []int) chan int {
 	}()
 	return chOut
 }
+
+func add(ctx context.Context, chIn chan int) chan int {
+	resChan := make(chan int)
+	go func() {
+		defer close(resChan)
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("done in add function")
+				return
+			case v, ok := <-chIn:
+				if !ok {
+					return
+				}
+				result := v + 13
+				select {
+				case <-ctx.Done():
+					fmt.Println("done in add function")
+				case resChan <- result:
+				}
+			}
+		}
+
+	}()
+
+	return resChan
+}
 func main() {
 	//задано какое-то множество данных в виде слайса
 	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1}
