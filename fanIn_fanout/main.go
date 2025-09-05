7a542a3c7e8fcb81c3b12c1ccc434ffbed083019
Vladimirmoscow84 <ccr1@yandex.ru>
2025-09-05 10:28:54 +0300

    generator done

--- fanIn_fanout/main.go
+++ fanIn_fanout/main.go
@@ -1,10 +1,35 @@
 /*
 Паттерны отвечающие за:
 fan-out - принимает канал с входными данными и запускает заданное множество горутин для одновременной обработки этих данных
-fan-in - обединяет несколько результатов в один канал (мультиплесирование)
+fan-in - обединяет несколько результатов c с разных канало в один канал (мультиплесирование)
 */
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
+// genrator - читает входные данные и отправляет их в канал
+func generator(ctx context.Context, data []int) chan int {
+	chOut := make(chan int)
+	go func() {
+		defer close(chOut)
+		for _, v := range data {
+			select {
+			case <-ctx.Done():
+				fmt.Println("done in generator")
+				return
+			case chOut <- v:
+			}
+		}
+	}()
+	return chOut
+}
+func main() {
+	//задано какое-то множество данных в виде слайса
+	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(1*time.Second))
+	defer cancel()
 }
