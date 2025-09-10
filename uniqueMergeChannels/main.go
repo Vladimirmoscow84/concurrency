c25049ce4625217ad9adfad319d73c95193fba90
Vladimirmoscow84 <ccr1@yandex.ru>
2025-09-11 00:34:53 +0300

    uniqueMerge continue

--- uniqueMergeChannels/main.go
+++ uniqueMergeChannels/main.go
@@ -10,3 +10,52 @@
 Результатом вашей функции будет канал, возвращающий следующую последовательность чисел: {1, 2, 3, 4, 5, 6, 7}, без повторений.
 */
 package main
+
+import (
+	"context"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	ch3 := make(chan int)
+	ch4 := make(chan int)
+
+	go func() {
+		defer close(ch1)
+		ch1 <- 3
+		ch1 <- 22
+		ch1 <- 23
+		ch1 <- 30
+	}()
+	go func() {
+		defer close(ch2)
+		ch2 <- 2
+		ch2 <- 3
+		ch2 <- 4
+		ch2 <- 7
+		ch2 <- 23
+	}()
+	go func() {
+		defer close(ch3)
+		ch3 <- 4
+		ch3 <- 7
+		ch3 <- 12
+		ch3 <- 22
+	}()
+	go func() {
+		defer close(ch4)
+		ch4 <- 2
+		ch4 <- 3
+		ch4 <- 7
+		ch4 <- 15
+		ch4 <- 16
+		ch4 <- 17
+		ch4 <- 23
+		ch4 <- 25
+	}()
+
+}
